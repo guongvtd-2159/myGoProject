eda6b5ad38b6e6651b73e19932f9d0a44761a591
guongvtd-2159 <vo.thi.dai.guong@sun-asterisk.com>
2025-05-22 21:39:09 +0700

    update code docker file

--- user-list/wait-for-mysql.sh
+++ user-list/wait-for-mysql.sh
@@ -2,15 +2,15 @@
 
 HOST="$1"
 PORT="$2"
-CMD="$3"
+shift 2
 
 echo "Waiting for MySQL at $HOST:$PORT..."
 
-# Vòng lặp cho tới khi kết nối được
+# Vòng lặp chờ MySQL
 while ! nc -z $HOST $PORT; do
   echo "Waiting for MySQL at $HOST:$PORT..."
   sleep 2
 done
 
 echo "MySQL is up - running the app"
-exec $CMD
+exec "$@"
